2a5b7d617365c385866f4f5fb0a92271e0b9ff1b
Rufus Mbugua <mbuguarufus@gmail.com>
2016-10-12 11:07:02 +0300

    Updates

--- controllers/bot.js
+++ controllers/bot.js
@@ -4,31 +4,37 @@ var TelegramBot = require('node-telegram-bot-api');
 
 var token = '241964818:AAFNMMqV1_nRuvA0D_nqz3zB15o77pMZsKI';
 // Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+var bot = new TelegramBot(token, {
+  polling: true,
+  webHook:{
+    port:8443,
+    key: './rufusmbugua.key',
+    cert: './rufusmbugua.pem'
+  }
+});
+bot.setWebHook('lucas.rufusmbugua.com/bot'+token, './rufusmbugua.pem');
+
+bot.onText(/\/echo (.+)/, function (msg, match) {
+  var fromId = msg.from.id;
+  var resp = match[1];
+  bot.sendMessage(fromId, resp);
+});
+
+// Any kind of message
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
+  // photo can be: a file path, a stream or a Telegram file_id
+  var message = "Wsup. I'm Lucas";
+  bot.sendMessage(chatId, message);
+});
 
 router.get('/', function(req, res) {
   var query = req.query;
   var message = query.message;
 
-  // res.send('Hello');
+  res.send(message);
   // // Matches /echo [whatever]
-  bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id;
-    var resp = match[1];
-    bot.sendMessage(fromId, resp);
-  });
 
-  // Any kind of message
-  bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    // photo can be: a file path, a stream or a Telegram file_id
-    var message = "Wsup. I'm Lucas";
-    bot.sendMessage(chatId, message);
-  });
 })
-
-router.get('/setWebhook', function(req, res) {
-  bot.setWebhook('bot.rufusmbugua.com','')
-});
 
 module.exports = router;
