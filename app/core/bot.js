e9d6547ae2ab2927e5d1e48e6f4ed6a8a4d42109
Rufus N Mbugua <mbuguarufus@gmail.com>
2018-03-09 16:21:55 +0300

    Added Procfile

--- app/core/bot.js
+++ app/core/bot.js
@@ -1,47 +1,58 @@
+'use strict';
 // Core Bot Framework
 // Load Modules
-var botkit = require('botkit');
-var Botmaster = require('botmaster');
-var express = require('express');
-var WebUIBot = require('./bots/web');
-var SlackBOT = require('slackbots');
-var TelegramBot = require('node-telegram-bot-api');
+const botkit = require('botkit');
+const Botmaster = require('botmaster');
+const express = require('express');
+const WebUIBot = require('./bots/web');
+const SlackBOT = require('slackbots');
+const TelegramBot = require('node-telegram-bot-api');
+const BootBot = require('bootbot');
 
-// Set Twitter Variables
-var twitterSettings = {
-  domain:'/',
-  credentials: {
-    consumerKey: process.env.TWITTER_consumerKey,
-    consumerSecret: process.env.TWITTER_consumerSecret,
-    accessToken: process.env.TWITTER_accessToken,
-    accessTokenSecret: process.env.TWITTER_accessTokenSecret,
-  }
-}
+// // Set Twitter Variables
+// const twitterSettings = {
+//   domain:'/',
+//   credentials: {
+//     consumerKey: process.env.TWITTER_consumerKey,
+//     consumerSecret: process.env.TWITTER_consumerSecret,
+//     accessToken: process.env.TWITTER_accessToken,
+//     accessTokenSecret: process.env.TWITTER_accessTokenSecret,
+//   }
+// }
+// 
+// 
 
-// Set Web Variables
-var webSettings = {
-  domain:'/',
-  webhookEndpoint:'/',
-  baseMessageURL:'http://localhost:3000/bot'
-}
+// // Set Web Variables
+// const webSettings = {
+//   domain:'/',
+//   webhookEndpoint:'/',
+//   baseMessageURL:'http://localhost:3000/bot'
+// }
 
-// Set Slack Variables
-var slackSettings = {
-    token: process.env.SLACK_BOT_TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token
-    name: process.env.SLACK_BOT_NAME
-}
+// // Set Slack Variables
+// const slackSettings = {
+//     token: process.env.SLACK_BOT_TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token
+//     name: process.env.SLACK_BOT_NAME
+// }
 
-//  Set Telegram Variables
-var telegramSettings = {
-  polling: true
-}
+// //  Set Telegram Variables
+// const telegramSettings = {
+//   polling: true
+// }
+
+const facebookSettings = {
+  accessToken: process.env.FB_ACCESS_TOKEN,
+  verifyToken: process.env.FB_VERIFY_TOKEN,
+  appSecret: process.env.FB_APP_SECRET
+};
 
 // Create Core Bot Object
-var bot = {
-  WebUIBot: new WebUIBot(webSettings),
-  TwitterBot: new Botmaster.botTypes.TwitterBot(twitterSettings),
-  SlackBot: new SlackBOT(slackSettings),
-  TelegramBot: new TelegramBot(process.env.TELEGRAM_BOT_TOKEN,telegramSettings)
+const bot = {
+    Messenger: new BootBot(facebookSettings)
+  // WebUIBot: new WebUIBot(webSettings),
+  // TwitterBot: new Botmaster.botTypes.TwitterBot(twitterSettings),
+  // SlackBot: new SlackBOT(slackSettings),
+  // TelegramBot: new TelegramBot(process.env.TELEGRAM_BOT_TOKEN,telegramSettings)
 }
 
 module.exports = bot;
